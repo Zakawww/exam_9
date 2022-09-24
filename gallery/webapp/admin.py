f11648752f2486dac983e38b1daf481530f28af7
Zakawww <zaka_333@mail.ru>
2022-09-24 12:50:38 +0600

    добавил модели и админ

--- gallery/webapp/admin.py
+++ gallery/webapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Photo, Comment, Like
 
-# Register your models here.
+admin.site.register(Photo)
+admin.site.register(Comment)
+admin.site.register(Like)
--- gallery/webapp/models.py
+++ gallery/webapp/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+    photo = models.ImageField(upload_to='photos', verbose_name='Фото')
+    note = models.CharField(max_length=100, verbose_name='Подпись')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    author = models.ForeignKey(User, related_name='photos', on_delete=models.CASCADE, verbose_name='Автор')
+    likes = models.IntegerField(default=0, verbose_name='Лайки')
+
+    def __str__(self):
+        return self.note
+
+
+class Like(models.Model):
+    author = models.ForeignKey(User, related_name='user_likes', on_delete=models.CASCADE, verbose_name='Автор')
+    photo = models.ForeignKey('webapp.Photo', related_name='photo_likes', on_delete=models.CASCADE, verbose_name='Фото')
+
+    class Meta:
+        unique_together = ('author', 'photo')
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=500, verbose_name='Текст')
+    photo = models.ForeignKey('webapp.Photo', related_name='photo_comments', on_delete=models.CASCADE,
+                              verbose_name='Фото')
+    author = models.ForeignKey(User, related_name='author_comments', on_delete=models.CASCADE, verbose_name='Автор')
+    create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.text
